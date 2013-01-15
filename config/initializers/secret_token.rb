daf30be14bf135d3665e81dc28536922816e18ad
kkambe <kkambe@kiwiup.com>
2013-01-15 23:28:22 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Actioncut::Application.config.secret_token = '99bb54161fe9b6985936a24aa5ae82db19ac60fbeef50da2121427243bf2b17c89cc115867e9b09d90dfee183154715071a1c8682a4b678420c8b80e58122bb7'
