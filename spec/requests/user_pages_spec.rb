6a43925490df553677add90a59f8529796895a5f
kkambe <kkambe@kiwiup.com>
2013-04-21 17:30:15 +0530

    Sign in, Sign out

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,7 +29,17 @@ describe "UserPages" do
       end
       it "should create a user" do
         expect {click_button submit}.to change(User, :count).by(1)
-      end  
+      end 
+      
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('test@gmail.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
+      end
+      
     end
   end
   
